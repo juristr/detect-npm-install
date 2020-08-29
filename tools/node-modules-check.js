d9ce6874a4affc67f53c6f53ca0307801f24b9a9
Juri <juri.strumpflohner@gmail.com>
2020-08-29 17:00:15 +0200

    optimize yarn / npm check

--- tools/node-modules-check.js
+++ tools/node-modules-check.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
 const shell = require('shelljs');
 const colors = require('colors');
+const fs = require('fs');
+
 const [
   NODE_PATH,
   SCRIPT_PATH,
   PREVIOUS_HEAD,
   CURRENT_HEAD,
   ISBRANCH,
-] = process.argv; // argv[node path, hook script path,  {what other args come from a merge action?}]
+] = process.argv;
 
-// Pulls file names for all changes from the original head to the current head.
 let changedFiles = shell.exec(
   'git diff-tree -r --name-only --no-commit-id ' +
     PREVIOUS_HEAD +
@@ -19,7 +20,13 @@ let changedFiles = shell.exec(
 
 // Checks to see if package.json has changed. If so, alerts user to run npm install.
 if (changedFiles.includes('package.json')) {
-  let msg = 'package.json changed: Please run "npm install"';
+  let msg = 'package.json changed: ';
+  if (fs.existsSync('yarn.lock')) {
+    msg += 'Please run "yarn install"';
+  } else {
+    msg += 'Please run "npm install"';
+  }
+
   let width = 80;
   console.log(
     colors.bold.inverse.yellow(
